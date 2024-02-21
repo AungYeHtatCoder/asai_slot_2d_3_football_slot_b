033ed0a22c2aa55fae2576b75bb0c2cccc9c3878
MayThinKyi303 <maythinkyi2003@gmail.com>
2024-02-21 21:16:07 +0630

    remove  wallet and login

--- src/components/Main/Navbar.jsx
+++ src/components/Main/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import '../../assets/css/Main/layout.css'
+import '../../assets/css/Main/layout.css';
+import logo from '../../assets/img/logo.png'
 import { IoNotificationsSharp } from "react-icons/io5";
 import { NavLink } from 'react-router-dom';
 import { GrLinkPrevious } from "react-icons/gr";
@@ -7,11 +8,13 @@ import {useLocation} from 'react-router-dom'
 const Navbar = () => {
   const location=useLocation();
   return (
-    <div className='mb-4 navbar p-4 d-flex justify-content-between align-items-center'>
+    <div className='mb-4 navbar px-4 d-flex justify-content-start gap-5 align-items-center'>
       {location.pathname!=='/lotto' && <GrLinkPrevious onClick={()=>history.back()} size={30} />}
-      <NavLink to={'/lotto'} className='text-decoration-none text-light' ><h5>Delight 2D | 3D</h5></NavLink>
+      <NavLink to={'/lotto'} className='text-decoration-none text-light' > 
+        <img src={logo} style={{height:'60px'}} />
+      </NavLink>
       {/* <IoNotificationsSharp size={25} /> */}
-      <NavLink to={'/lotto/login'} className="btn px-3 rounded text-white" style={{border:'1px solid #a202ff'}}>Login</NavLink>
+      {/* <NavLink to={'/lotto/login'} className="btn px-3 rounded text-white" style={{border:'1px solid #a202ff'}}>Login</NavLink> */}
     </div>
   )
 }
