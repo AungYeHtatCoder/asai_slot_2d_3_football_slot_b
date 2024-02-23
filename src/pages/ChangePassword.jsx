c79c7661052ab672c8fd017ed6ff48021c07c5f5
ThienTheinSoe <theintheinose1218@gmail.com>
2024-02-22 21:50:50 -0800

    change password api integrate

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -1,39 +1,103 @@
-import React from 'react'
+import React, { useState } from "react";
 import { FaLock } from "react-icons/fa";
-import '../assets/css/changePassword.css'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import "../assets/css/changePassword.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import BASE_URL from "../hooks/baseURL";
+import { Alert } from "react-bootstrap";
 
 function ChangePasswordPage() {
-  return (<>
-  <div className="cgPwTitle py-3 ">
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+
+  const handlePassword = (e) => {
+    e.preventDefault();
+    const inputData = {
+      currentPassword: currentPassword,
+      password: password,
+    };
+    // console.log(inputData);
+    fetch(BASE_URL + "/changePassword", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("authToken"),
+      },
+      body: JSON.stringify(inputData),
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          let errorData;
+          try {
+            errorData = await response.json();
+          } catch (error) {
+            console.error("Error parsing JSON:", error);
+          }
+          if (response.status === 422) {
+            setError(errorData.errors);
+            console.error(`${response.status}:`, errorData);
+          } else if (response.status === 401) {
+            setError(errorData.message);
+            console.error(`${response.status}:`, errorData);
+          } else {
+            console.error(`Unexpected error with status ${response.status}`);
+          }
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setSuccess("New Password Changed Successfully.");
+        setCurrentPassword("");
+        setPassword("");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <>
+      {/* <div className="cgPwTitle py-3 ">
         <FaLock /> လျှို့ဝှတ်နံပါတ်အားမူလအတိုင်းပြန်လုပ်သည်
-        </div>
-        <Form className='p-4 cgPwForm'>
+      </div> */}
+      {success && <Alert variant="success">{success}</Alert>}
+      <Form className="p-4 cgPwForm" onSubmit={handlePassword}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>စကားဝှက်အသစ်</Form.Label>
-            <Form.Control type="password" placeholder="စကားဝှက်အသစ်" />
+          <Form.Label>စကားဝှက်အဟောင်း</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="စကားဝှက်အဟောင်း"
+            onChange={(e) => setCurrentPassword(e.target.value)}
+            value={currentPassword}
+          />
+          {error.currentPassword && (
+            <span className="text-danger">*{error.currentPassword}</span>
+          )}
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>စကားဝှက်အသစ်ကို အတည်ပြုပါ။</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Label>စကားဝှက်အသစ်</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="စကားဝှက်အသစ်"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </Form.Group>
         <div className="cgPwBtn">
-            <Button  variant="warning" type="submit">
+          <Button variant="warning" type="submit">
             အတည်ပြုပါ။
-            </Button>
+          </Button>
         </div>
-
-        
-        </Form>
+      </Form>
     </>
   );
 }
 
 export default ChangePasswordPage;
 
-
 //  လျှို့ဝှတ်နံပါတ်အားမူလအတိုင်းပြန်လုပ်သည်
 // စကားဝှက်အသစ်
 // စကားဝှက်အသစ်ကို အတည်ပြုပါ။
-
